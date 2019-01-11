2f25a9edc1beb724a2de896edca29b43553ec6a5
vitalii <vitalik@gmail.com>
2019-01-11 04:30:45 +0200

    vksdnfvjsafbsdvs

--- js/common.js
+++ js/common.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
 	$(document).ready(function(){
 		$('.btn-up').mPageScroll2id();
 	});
-	
+	$(".btn-order-info").click(function(){
+		$(".alert-vidpravka").css("display","block");
+	});
+	$(".zakrutu").click(function(){
+		$(".alert-vidpravka").css("display","none");
+	});
 });
